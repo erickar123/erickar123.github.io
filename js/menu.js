7d51be7fb1abfa276560debdd88251b8b25ab6f4
erickar123 <erick.arr1999@gmail.com>
2025-03-26 03:44:18 -0600

    subir website
    he subido todos mis archivos

---
+++ js/menu.js
@@ -0,0 +1,21 @@
+(function(){
+    const openButton = document.querySelector('.nav__menu');
+    const menu = document.querySelector('.nav__link');
+    const closeMenu = document.querySelector('.nav__close');
+    const menuLinks = document.querySelectorAll('.nav__link a[href^="#"]');
+
+    openButton.addEventListener('click', ()=>{
+        menu.classList.add('nav__link--show');
+    });
+
+    closeMenu.addEventListener('click', ()=>{
+        menu.classList.remove('nav__link--show');
+    });
+
+    menuLinks.forEach(menuLinks =>{
+        menuLinks.addEventListener('click', function() {
+            menu.classList.remove('menu');
+        })
+    });
+
+})();
